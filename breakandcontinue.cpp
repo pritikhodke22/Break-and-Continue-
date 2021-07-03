c88c1719166961a0739cd8f934ef7c6bbc8863eb
Priti Khodke <72237736+pritikhodke22@users.noreply.github.com>
2021-07-03 08:31:44 -0700

    Add files via upload

---
+++ breakandcontinue.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int date=1;date<=30;date++)
+    {
+        if(date%2==0)
+        {
+            continue;
+        }
+        cout<<"Go out"<<endl;
+    }
+    return 0;
+}
